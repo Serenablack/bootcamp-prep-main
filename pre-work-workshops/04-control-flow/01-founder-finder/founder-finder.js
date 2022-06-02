fee2e28332ba099ad088689f3053f8455b684058
Serena Black <sunsetsin1.sy@gmail.com>
2022-06-02 13:50:54 +0545

    solved problems in control flow

--- pre-work-workshops/04-control-flow/01-founder-finder/founder-finder.js
+++ pre-work-workshops/04-control-flow/01-founder-finder/founder-finder.js
@@ -1,7 +1,12 @@
-let name = 'David';
+let name = "David";
 // let name = 'Nimit';
 // let name = 'Someone else';
 
 let found;
 
 // YOUR CODE BELOW
+if ("David" || "Nimit") {
+  found = true;
+} else if ("Someone else") {
+  found = false;
+}
