711374464915c8016ad35e17a48a969e7f39f56b
Serena Black <sunsetsin1.sy@gmail.com>
2022-06-20 11:40:27 +0545

    solved questions related to higher order functions i and ii and recursion i

--- workshops/12-recursion-i/07-is-palindrome/is-palindrome.js
+++ workshops/12-recursion-i/07-is-palindrome/is-palindrome.js
@@ -1,1 +1,15 @@
 // YOUR CODE BELOW
+function isPalindrome(str) {
+  if (str.length === 1) {
+    return str;
+  }
+  debugger;
+  let result = str[str.length - 1];
+  str = str.slice(0, -1);
+  result = result.concat(isPalindrome(str));
+
+  if (str2.toLowerCase() === result.toLowerCase()) {
+    return true;
+  } else return false;
+}
+isPalindrome("abbba");
